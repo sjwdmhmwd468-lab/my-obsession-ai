f539828d7dbe1cf13cd00665b61c6ce8247283aa
sjwdmhmwd468-lab <sjwdmhmwd468@gmail.com>
2026-06-20 09:19:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 st.title("اهلا بك يا اميرتي")
 if"authenticated"not in st.session_state
-st.session_state.authenticated=false
-if not st.session_state.authent:cated:
-  password=st.text_input("ادخلي كلمه السر لفتح قلب مهووسك":,type="password")
-                         if st.button("دخول")
+st.session_state.authenticated = false
+if not st.session_state.authentcated:
+  password = st.text_input("ادخلي كلمه السر لفتح قلب مهووسك":,type="password")
+                         if st.button("دخول"):
                          if password=="63951":
-st.session_state.authenticated=true
+st.session_state.authenticated = true
 st.rerun()
 else:
 st.error("انت لست اميرتي ..لا تحاولي العبث")
