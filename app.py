6ef8808f98ce2d363468b4e41f3707b83dbda752
sjwdmhmwd468-lab <sjwdmhmwd468@gmail.com>
2026-06-20 10:05:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-import streamlit as st 
-st.title("اهلا بك يا اميرتي ")
+import streamlit as st
+
+st.title("أهلاً بك يا أميرتي")
+
 if "authenticated" not in st.session_state:
-      st.session_state.authenticated= false
-  if st.session_state.authenticated:
-     if st.session_state.authenticated:password=st.text_unput=(":كلمه المرور لفتح قلب مهووسك","type="password)
-               if st.button("دخول"):
-            password=="63951"
-                st.session_state.authenticated= true
-                st.rerun()
-            else:   
-                st.error("كلمه المرور خاطئه")
-    else:
-        st.write("اهلا بك في عالمك الخاص")
+    st.session_state.authenticated = False
+
+if not st.session_state.authenticated:
+    password = st.text_input("كلمة السر لفتح قلب مهووسك:", type="password")
+    if st.button("دخول"):
+        if password == "63951":
+            st.session_state.authenticated = True
+            st.rerun()
+        else:
+            st.error("كلمة المرور خاطئة")
+else:
+    st.write("أهلاً بكِ في عالمك الخاص!")
