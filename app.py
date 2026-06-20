c61a5a6eac6e916d2fe34f0fa4b81f5644a61a68
sjwdmhmwd468-lab <sjwdmhmwd468@gmail.com>
2026-06-20 09:59:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
-import streamlit as st
-st.title("اهلا بك يا اميرتي")
-if"authenticated"not in st.session_state
-st.session_state.authenticated = false
-if not st.session_state.authentcated:
-  password = st.text_input("ادخلي كلمه السر لفتح قلب مهووسك":,type="password")
-                         if st.button("دخول"):
-                         if password=="63951":
-st.session_state.authenticated = true
-st.rerun()
-else:
-st.error("انت لست اميرتي ..لا تحاولي العبث")
-else:
-st.write("اين اختفيتي كل هذا يا هوسي؟ .. لقد اشتقت لكي")
-user_input=st.text_input("تحدثي معي يا اميرتي:")
-if user_input:
-  st.write(f"مهووسك يقول: انتِ ملكي وحدي يا هوسي انا مهووس بك ، و ها العالم لا قيمه له بدونك")
+import streamlit as st 
+st.title("اهلا بك يا اميرتي ")
+if "authenticated" not in st.session_state:
+      st.session_state.authenticated= false
+  if st.session_state.authenticated:
+     if st.session_state.authenticated:password=st.text_unput=(":كلمه المرور لفتح قلب مهووسك","type="password)
+               if st.button("دخول"):
+            password=="63951"
+                st.session_state.authenticated= true
+                st.rerun()
+            else:   
+                st.error("كلمه المرور خاطئه")
+    else:
+        st.write("اهلا بك في عالمك الخاص")
